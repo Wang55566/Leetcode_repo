aeb42b30b134bbefc00399818835967e9863d51b
Wang55566 <wang6@hawaii.edu>
2023-11-22 14:47:30 -0800

    added sorted function

--- binary_search/888. Fair Candy Swap.py
+++ binary_search/888. Fair Candy Swap.py
@@ -6,19 +6,20 @@ class Solution:
         total_alice = sum(aliceSizes)
         total_bob = sum(bobSizes)
 
-        total_difference = total_bob - total_alice  #2
-        target = total_difference // 2  # 2/2 = 1
+        bobSizes.sort()
 
-        for candy in aliceSizes:  # [1, 1]
-            real_target = candy + target   # 1 + 1 = 2
-            # real_target = total_difference - candy
-            print(candy, target)
+        total_difference = total_bob - total_alice
+        target = total_difference // 2
+
+        for candy in aliceSizes:
+            real_target = candy + target
             left = 0
             right = len(bobSizes) - 1
 
             while left <= right:
+
                 middle = (left + right) // 2
-                # print(left, middle, right, bobSizes[middle], real_target)
+
                 if bobSizes[middle] == real_target:
                     return [candy, real_target]
                 elif bobSizes[middle] > real_target:
