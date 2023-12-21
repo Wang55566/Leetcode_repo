c682019fea741422618d046e5084376fd48bd4b8
Wang55566 <wang6@hawaii.edu>
2023-12-20 16:10:41 -0800

    commit

---
+++ BFS/617. Merge Two Binary Trees.py
@@ -0,0 +1,38 @@
+from collections import deque
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+
+        deque1 = deque([root1])
+        deque2 = deque([root2])
+
+        while deque1 and deque2:
+            node1 = deque1.popleft()
+            node2 = deque2.popleft()
+
+            node1.val += node2.val
+
+            if node1.left and node2.left:
+                deque1.append(node1.left)
+                deque2.append(node2.left)
+            elif node2.left:
+                node1.left = node2.left
+
+            if node1.right and node2.right:
+                deque1.append(node1.right)
+                deque2.append(node2.right)
+            elif node2.right:
+                node1.right = node2.right
+
+        return root1
