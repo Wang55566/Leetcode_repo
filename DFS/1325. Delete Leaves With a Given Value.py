cb662066326d3b6ced2399d86f0250ad589ecb50
Wang55566 <wang6@hawaii.edu>
2024-01-31 16:58:20 -0800

    commit

---
+++ DFS/1325. Delete Leaves With a Given Value.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+
+        def recursion(currNode, parentNode, direction):
+            if currNode:
+                currNode.left = recursion(currNode.left, currNode, "left")
+                currNode.right = recursion(currNode.right,currNode, "right")
+                if currNode.val == target and not currNode.left and not currNode.right:
+                    if not parentNode:
+                        return None
+
+                    if direction == "left":
+                        parentNode.left = None
+                    else:
+                        parentNode.right = None
+                    return
+
+            return currNode
